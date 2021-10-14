e41b31a21081d2565e201f90ae6f484f58723c6a
xiaofeng-H <2220342736@qq.com>
2021-10-14 11:44:03 +0800

    数据结构与算法：Dijkstra算法和Floyd算法。

--- dataStructuresAndAlgorithms/algorithms/graph.go
+++ dataStructuresAndAlgorithms/algorithms/graph.go
@@ -1,6 +1,7 @@
 package algorithms
 
 import (
+	"fmt"
 	ds "study/dataStructuresAndAlgorithms/dataStructures"
 )
 
@@ -100,4 +101,116 @@ func Kruskal(g *ds.MGraph, road []Road) (sum int32) {
 // 排序算法
 func sort(roads []Road, num int32) {
 
+}
+
+// 最短路径：迪杰斯特拉算法
+/*
+1.数据结构：邻接矩阵
+2.时间复杂度：O(n^2)
+3.运行结果：函数结束时，dist[]数组中存放了v0点到其余顶点的最短路径长度，path[]中存放了v0点到其余顶点的最短路径
+*/
+func Dijkstra(g *ds.MGraph, v0 int32) ([]int32, []int32) {
+	// 记录当前已找到的从v0到每个终点vi的最短路径长度
+	// 其初始状态为：若v0到vi有边，则dist[i]为边上的权值，否则置为无穷大
+	var dist []int32 = make([]int32, 0)
+	// 顶点标记数组，vSet[i]=0表示顶点i已经被并入最短路径，vSet[i]=1则表示顶点i还未被并入最短路径
+	var vSet []int32 = make([]int32, 0)
+	// 保存从v0到vi最短路径上vi的前一个顶点，其初始状态为：若v0到vi有边，则path[vi]=v0，否则path[vi]=-1
+	var path []int32 = make([]int32, 0)
+
+	// 1.数据初始化
+	for i := int32(0); i < g.N-1; i++ {
+		if g.Edges[v0][i] == 0 {
+			dist[i] = INF
+			path[i] = -1
+		} else {
+			dist[i] = g.Edges[v0][i]
+			path[i] = v0
+		}
+		vSet[i] = 0
+	}
+
+	// 2.以v0为起始点，开始寻找各个顶点的最短路径
+	vSet[v0] = 1 // 顶点v0作为起始点，第一个被并入最短路径
+	for i := int32(0); i < g.N; i++ {
+		min := int32(INF) // 权值最小的下一条路径上的权值
+		var v = int32(0)  // 下一个将被并入最段路径的顶点
+		// 3.该循环每次从剩余顶点中选出一个顶点，通往这个顶点的路径在通往所有剩余顶点的路径中是长度最短的
+		for i := int32(0); i < g.N; i++ {
+			if vSet[i] == 0 && dist[i] < min {
+				min = dist[i] // 记录最小权值
+				v = i         // 记录权值最小的顶点的编号
+			}
+		}
+
+		// 4.将选出的顶点并入最短路径中，同时更新各个辅助数组的数据
+		vSet[v] = 1
+		// 该循环以刚并入的顶点作为中间点，对所有通往剩余顶点的路径进行检测
+		for i := int32(0); i < g.N; i++ {
+			// 判断顶点v的加入是否会出现通往顶点i的更短路径，如果出现，则改变原来路径及其长度
+			if vSet[i] == 0 && dist[i] > dist[v]+g.Edges[v][i] {
+				dist[i] = dist[v] + g.Edges[v][i]
+				path[i] = v
+			}
+		}
+	}
+	// 5.返回最短路径及路径数组
+	return dist, path
+}
+
+// path[]数组最短路径打印
+func PrintPath(path [MAXSIZE]int32, a int32) {
+	// 借助栈来实现逆向输出
+	var stack [MAXSIZE]int32
+	var top = int32(-1)
+	// 该循环以由叶子结点到根结点的顺序将其入栈
+	for path[a] != -1 {
+		top++
+		stack[top] = a
+		a = path[a]
+	}
+	top++
+	stack[top] = a
+	// 打印
+	for top != -1 {
+		v := stack[top]
+		top--
+		fmt.Printf("%d, ", v)
+	}
+	fmt.Println()
+}
+
+// 最短路径：佛洛伊德算法
+/*
+1.数据结构：邻接矩阵
+2.时间复杂度：O(n^3)
+*/
+func Floyd(g *ds.MGraph) ([][]int32, [][]int32) {
+	// arr用来记录当前已经求得的任意两个顶点最短路径的长度
+	var arr [][]int32 = make([][]int32, 0)
+	// path用来记录当前两顶点间最短路径上要经过的中间顶点
+	var path [][]int32 = make([][]int32, 0)
+
+	// 1.数据初始化
+	for i := int32(0); i < g.N; i++ {
+		for j := int32(0); j < g.N; j++ {
+			arr[i][j] = g.Edges[i][j]
+			path[i][j] = -1
+		}
+	}
+
+	// 2.以k为中间点，对所有的顶点对<i,j>进行检测
+	for k := int32(0); k < g.N; k++ {
+		for i := int32(0); i < g.N; i++ {
+			for j := int32(0); j < g.N; j++ {
+				if arr[i][j] > arr[i][k]+arr[k][j] {
+					arr[i][j] = arr[i][k] + arr[k][j]
+					path[i][j] = k
+				}
+			}
+		}
+	}
+
+	// 返回最短路径及路径
+	return arr, path
 }
